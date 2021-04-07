237bbf51696720517cb26389cb122febcf41d4d5
Juan De Dios <jbaudazio@gmail.com>
2021-04-07 01:17:28 -0500

    modificar el archivo bat

--- build/resources/test/startOWASP.bat
+++ build/resources/test/startOWASP.bat
@@ -5,5 +5,5 @@ ZAP.exe
 ping 127.0.0.1
 
 E:
-cd E:\Cursos\JB\Vulnerabilidades\Clase02\Automatizacion\Scripts\Clase01
+cd E:\Cursos\JB\Vulnerabilidades\Talleres\Taller3\Clase01
 gradle clean test
--- src/test/resources/startOWASP.bat
+++ src/test/resources/startOWASP.bat
@@ -2,8 +2,8 @@ c:
 cd "C:\Program Files\OWASP\Zed Attack Proxy"
 ZAP.exe
 
-ping 127.0.0.1 -t10
+ping 127.0.0.1
 
 E:
-cd E:\Cursos\JB\Vulnerabilidades\Clase02\Automatizacion\Scripts\Clase01
+cd E:\Cursos\JB\Vulnerabilidades\Talleres\Taller3\Clase01
 gradle clean test
